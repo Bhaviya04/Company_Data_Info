eb942398c206ab303cfdf2fdbf7fdf8ad416850a
unknown <bhaviya04@csu.fullerton.edu>
2018-02-15 19:39:51 -0800

    Project on ASP.NET MVC and Entity Framework with multiple Models in a Single View

---
+++ ASP_NET_MVC_Entity_Framework/ASP_NET_MVC_Entity_Framework/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_NET_MVC_Entity_Framework.Models;
+using System.Dynamic;
+
+namespace ASP_NET_MVC_Entity_Framework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MVC_Entity_FrameworkEntities entities = new MVC_Entity_FrameworkEntities();
+            dynamic details = new ExpandoObject();
+            details.abc = (from customer in entities.Customers.Take(10) select customer).ToList();
+            details.xyz = (from staff in entities.Staffs.Take(10) select staff).ToList();
+            return View(details);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASP_NET_MVC_Entity_Framework/ASP_NET_MVC_Entity_Framework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_NET_MVC_Entity_Framework.Startup))]
+namespace ASP_NET_MVC_Entity_Framework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
